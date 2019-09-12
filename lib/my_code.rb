bb8172685acae033ad759d1550b9085bd1a66ef3
Melissa Christman <melchrstmn@gmail.com>
2019-09-12 12:35:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,7 +51,8 @@ end
 def reduce_to_all_true(array)
   counter = 0
   while counter < array.size do
-    return false if 
+    if source_array.include? (false)
+    return false
     counter += 1 
   end
   return true
