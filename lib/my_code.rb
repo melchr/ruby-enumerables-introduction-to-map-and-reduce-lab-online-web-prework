d283fbb302dcfff5b8a8cad119a40a47cc9f0d11
Melissa Christman <melchrstmn@gmail.com>
2019-09-12 12:28:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,6 +48,16 @@ def reduce_to_total(array, starting_point=0)
   total
 end
 
+def reduce_to_total(array, starting_point=0)
+  total = starting_point
+  counter = 0
+  while counter < array.size do
+    total += array[counter]
+    counter += 1 
+  end
+  total
+end
+
 
 
 
