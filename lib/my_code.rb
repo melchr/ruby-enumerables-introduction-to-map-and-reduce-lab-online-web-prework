984619c6575191244428026653a3177974ad1da5
Melissa Christman <melchrstmn@gmail.com>
2019-09-12 12:32:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -57,7 +57,7 @@ def reduce_to_all_true(array)
   return true
 end
 
-def reduce_to_all_true(array)
+def reduce_to_any_true(array)
   counter = 0
   while counter < array.size do
     return false if 
